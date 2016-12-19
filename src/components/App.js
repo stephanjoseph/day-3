010da42c73830903e44d1e0a5128a6005950a362
Stephan Joseph <stephanjoseph2007@gmail.com>
2016-12-19 18:05:38 -0500

    Working on header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react'
 import styles from '../styles/screen.scss'
 
-const images = {
-  code: require('./images/code.png'),
-  react: require('./images/react.png')
-}
-
 class App extends Component {
 
   render () {
@@ -14,6 +9,7 @@ class App extends Component {
         <section className='intro'>
           <header className='page'>
             <img className='stephan' src='https://avatars.githubusercontent.com/u/22772275?v=3' alt='stephan' />
+            <img className='code' src='https://static.pexels.com/photos/239898/pexels-photo-239898.jpeg' alt='code' />
             <blockquote>
               <h1> Hi, I'm Stephan Joseph</h1>
               <p> "I'm a junior front end engineer in Palm Harbor, Florida" </p>
@@ -23,22 +19,22 @@ class App extends Component {
 
         <section />
         <article className='top'>
-          <img className='code' src={images.code} alt='code' />
+          <img className='mountains' src='https://static.pexels.com/photos/239898/pexels-photo-239898.jpeg' alt='mountains' />
 
           <h1> Hello, React </h1>
-          <time> December 1th 2017</time>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum rerum officia quidem assumenda necessitatibus quibusdam dolor sequi molestiae aliquid inventore ut optio quisquam mollitia, repellendus, nihil, debitis voluptate aliquam eum?</p>
           <a className='more' href='#'>Keep Reading</a>
 
         </article>
 
         <article className='bottom'>
-          <img className='react' src={images.react} alt='react' />
+          <img className='stars' src='https://static.pexels.com/photos/110252/pexels-photo-110252.jpeg' alt='stars' />
           <h1> Fizz Buzz </h1>
           <time> December 6th 2017</time>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum rerum officia quidem assumenda necessitatibus quibusdam dolor sequi molestiae aliquid inventore ut optio quisquam mollitia, repellendus, nihil, debitis voluptate aliquam eum?</p>
           <a className='more' href='#'>Keep Reading</a>
         </article>
+
       </div>
     )
   }
